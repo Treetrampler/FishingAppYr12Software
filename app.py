4fac0b197afb049db3250a3ceddf33d66b8d9ae4
Treetrampler <treetrampler@gmail.com>
2025-02-04 21:50:13 +1100

    first login frontend

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
