20cc94bb8d4890276160c0d0042428d461aba0b0
Treetrampler <treetrampler@gmail.com>
2025-02-05 08:58:33 +1100

    nav bar UI set up

--- app.py
+++ app.py
@@ -7,8 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/access_login_page', methods=['GET'])
+def access_login_page():
+    return render_template('login_page.html')
+
+
+@app.route('/fish_identifier', methods=['GET'])
+def fish_identifier():
+    pass
+
+@app.route('/map', methods=['GET'])
+def map():
+    pass
+
 @app.route('/login', methods=['POST'])
 def login():
+    pass
+
+@app.route('/fish_dex', methods=['GET'])
+def fish_dex():
     pass
 
 if __name__ == '__main__':
